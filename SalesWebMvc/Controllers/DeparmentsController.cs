df151365fc6d7be47d70714bb7d8b0615b589959
Diego Santana <3c.santana@gmail.com>
2024-02-13 12:12:22 -0300

    Primeiro MVC - Department

---
+++ SalesWebMvc/Controllers/DeparmentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using System.Collections.Generic;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DeparmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Deparment> list = new List<Deparment>();
+            list.Add(new Deparment() { Id = 1, Name = "Electronics" });
+            list.Add(new Deparment() { Id = 2, Name = "Fashion" });
+
+            return View(list);
+        }
+    }
+}
