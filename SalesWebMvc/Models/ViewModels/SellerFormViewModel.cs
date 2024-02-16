435850152f2ec849649968de1deb1bd588b60689
Diego Santana <3c.santana@gmail.com>
2024-02-16 12:08:16 -0300

    feat: cria um ViewModel para o form de vendedores

---
+++ SalesWebMvc/Models/ViewModels/SellerFormViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models.ViewModels
+{
+    public class SellerFormViewModel
+    {
+        public ICollection<Department> Departments { get; set; }
+        public Seller Seller { get; set; }
+
+        public SellerFormViewModel(ICollection<Department> departmens)
+        {
+            Departments = departmens;
+        }
+    }
+}
